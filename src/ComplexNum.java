cffb1fea93f0ced3df3fee82a82dfadb33e0d1aa
Stanislaw <stas33933@gmail.com>
2025-03-17 22:29:36 +0300

    1.3

--- src/ComplexNum.java
+++ src/ComplexNum.java
@@ -2,26 +2,26 @@ public class ComplexNum {
     private final double re;
     private final double im;
 
-    public ComplexNum(double x, double y){
-        this.re = x;
-        this.im = y;
+    public ComplexNum(double re, double im){
+        this.re = re;
+        this.im = im;
     }
 
-    public double getX() {
+    public double getRe() {
         return re;
     }
 
-    public double getY(){
+    public double getIm(){
         return im;
     }
 
     @Override
     public String toString(){
-        return "Это комплексное число. Действительная часть = " + getX() + ", Мнимая = " + im + "i";
+        return "Это комплексное число. Действительная часть = " + getRe() + ", Мнимая = " + getIm() + "i";
     }
 
     public double abs(){
-        return Math.sqrt(getX() * getX() + getY() * getY());
+        return Math.sqrt(getRe() * getRe() + getIm() * getIm());
     }
 
     public double argueNum() throws Exception{
@@ -29,9 +29,47 @@ public class ComplexNum {
             throw new IllegalArgumentException("Модуль равен нулю!");
         }
         else{
-            return Math.acos(getX() / abs());
-            //return Math.asin(getY() / abs());
+            return Math.acos(getRe() / abs());
+            //return Math.asin(getIm() / abs());
         }
+    }
+
+    public ComplexNum sum(ComplexNum num){
+        return new ComplexNum(num.getRe() + getRe(), num.getIm() + getIm());
+    }
+
+    public ComplexNum difference(ComplexNum num){
+        return new ComplexNum(num.getRe() - getRe(), num.getIm() - getIm());
+    }
+
+    public ComplexNum increase(ComplexNum num){
+        return new ComplexNum((num.getRe() * getRe() - num.getIm() * getIm()), (getIm() * num.getRe() + getRe() * num.getIm()));
+    }
+
+    public ComplexNum division(ComplexNum num){
+        return new ComplexNum((getRe() * num.getRe() + getIm() * num.getIm()) / Math.pow(num.getIm(), 2) + Math.pow(num.getRe(), 2), (getIm() * num.getRe() - getRe() * num.getIm()) / Math.pow(num.getRe(), 2) + Math.pow(num.getIm() , 2));
+    }
+
+    public double trigonometryArgue() {
+        if(getRe() > 0 && getIm() > 0){
+            return Math.atan(getRe() / getIm());
+        }
+        else if(getRe() < 0 && getIm() > 0){
+            return Math.PI - Math.atan(getIm() / Math.abs(getRe()));
+        }
+        else if(getRe() < 0 && getIm() < 0){
+            return -1 * Math.PI + Math.atan(getIm() / getRe());
+        }
+        else if(getRe() > 0 && getIm() < 0){
+            return -1 * Math.atan(Math.abs(getIm()/getRe()));
+        }
+        else{
+            throw new IllegalArgumentException("Действительная или мнимая часть числа равна нулю!");
+        }
+    }
+
+    public double trigonometryForm(){
+        return abs() * (Math.cos(trigonometryArgue()) + Math.sin(trigonometryArgue()));
     }
 }
 
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,16 @@
+
 public class Main {
-    public static void main(String[] args) {
-        ComplexOperations operations = new ComplexOperations();
-        ComplexTrigonometry trigonometry = new ComplexTrigonometry();
+    public static void main(String[] args) throws Exception {
         ComplexNum numOne = new ComplexNum(-12, -10);
         ComplexNum numTwo = new ComplexNum(12, 3);
         System.out.println(numOne);
         System.out.println(numTwo);
-        double result = trigonometry.trigonometryForm(numOne);
+
+        ComplexNum sum = numOne.sum(numTwo);
+        System.out.println(sum);
+
+        double result = numOne.trigonometryForm();
         System.out.println(result);
-        ComplexNum numSum = operations.sum(numOne, numTwo);
-        System.out.println(numSum);
 
     }
 }
